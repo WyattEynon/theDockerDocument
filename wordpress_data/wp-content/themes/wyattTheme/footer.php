c32fa03b60f32ee11f888b7ee9e710ec750ff8b8
Wyatt <wyatteynon@gmail.com>
2021-10-02 16:17:09 -0400

    Uploaded files in an unzipped format. Added Pics

---
+++ wordpress_data/wp-content/themes/wyattTheme/footer.php
@@ -0,0 +1,7 @@
+<div id= "ttr_footer">
+    <br>
+    <h4>Where Docker is the Destination</h4>
+</div>
+</div>
+</body>
+</html>
---
+++ wordpress_data/wp-content/themes/wyattTheme/header.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+    <title>Tutorial theme</title>
+    <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/jquery.js'; ?>">
+    </script>
+    <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/jquery-ui.min.js'; ?>">
+    </script>
+    <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/bootstrap.js'; ?>">
+    </script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri().'/css/bootstrap.css'; ?>">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+</head>
+
+<body>
+
+<div id="ttr_header" class="jumbotron">
+    <h1>The Docker Document</h1>
+    <p>Shenanigans. Docker. Nematodes.</p>
+
+</div>
+<ul id="headerNavBar" class="nav justify-content-center">
+    <li class="nav-item">
+        <a class="nav-link" href="http://localhost:8000/?page_id=28">Kub</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="http://localhost:8000/?page_id=25">Grooves</a>
+    </li>
+    <li class="News">
+        <a class="nav-link" href="http://localhost:8000/?page_id=19">Link</a>
+    </li>
+</ul>
+
+<div class="container">
